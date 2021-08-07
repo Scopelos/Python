55b33fd1925822147b023ecd3081fc4ff5d968c1
Scopelos <88057360+Scopelos@users.noreply.github.com>
2021-08-08 00:50:30 +0300

    Update task_5.py

--- PythonLesson_3/task_5.py
+++ PythonLesson_3/task_5.py
@@ -1,9 +1,18 @@
 def my_func_1():
-    a = input("Введите несколько целых чисел через пробел: ")
-    b = map(int, input(a).split())
-    print(b)
-    for x in b:
-        c = sum(x)
-    return c
+    a = list(map(int, input("Введите несколько целых чисел через пробел: ").split()))
+    b = sum(a)
+    return b
+c = my_func_1()
+print(c)
 
-print(my_func_1())
+def my_func_2():
+    d = list(map(int, input("Введите несколько целых чисел через пробел: ").split()))
+    e = sum(d)
+    return e
+f = my_func_2()
+print(f)
+
+g = c + f
+print(g)
+
+print("Не получилось сделать последнее условие ни через break, ни через exception")
